76e91a393343397832a0845c2ab91fdf2273b4d0
Ali Athathi <aliathathi@gmail.com>
2023-03-07 15:55:23 +0300

    update function file -add redirct and getimage and checkextentions

--- app/core/functions.php
+++ app/core/functions.php
@@ -3,6 +3,31 @@ defined('ROOTPATH') or exit('<h1 style="margin-top:20px;text-align:center">Acces
 /**
  * customs function 
  */
+checkExtensions();
+function checkExtensions()
+{
+    $required_extensions = [
+        'gd',
+        'mysqli',
+        'pdo_mysql',
+        'pdo_sqlite',
+        'curl',
+        'fileinfo',
+        'intl',
+        'exif',
+        'mbstring'
+    ];
+    $not_loaded = [];
+    foreach ($required_extensions as $ext) {
+        if (!extension_loaded($ext)) {
+            $not_loaded[] = $ext;
+        }
+    }
+    if (!empty($not_loaded)) {
+        show("Please load the following extensions in your php.ini file:<br>" . implode("<br>", $not_loaded));
+        die();
+    }
+}
 function show($staff)
 {
     echo '<pre>';
@@ -12,4 +37,32 @@ function show($staff)
 function esc($str)
 {
     return htmlspecialchars($str);
+}
+function redirect($path)
+{
+    header('location: ' . ROOT . '/' . $path);
+    die;
+}
+/** load image. if not exist, load placeholder **/
+function getImage(mixed $file = '', string $type = 'post'): string
+{
+    $file = $file ?? '';
+    if (file_exists($file)) {
+        return ROOT . '/' . $file;
+    }
+    if ($type == 'user') {
+        return ROOT . '/assets/images/user.png';
+    } else {
+        return ROOT . '/assets/images/no_image.jpg';
+    }
+}
+/** return pagenation links */
+function getPaginationVars(): array
+{
+    $vars = [];
+    $vars['page']       = $_GET['page'] ?? 1;
+    $vars['page']       = (int)$vars['page'];
+    $vars['prev_page']  = $vars['page'] <= 1 ? 1 : $vars['page'] - 1;
+    $vars['next_page']  = $vars['page'] + 1;
+    return $vars;
 }
--- public/index.php
+++ public/index.php
@@ -1,10 +1,16 @@
 <?php
 session_start();
+
+/** Valid PHP Version ? */
+$minPPHVersion = '8.0';
+if (phpversion() < $minPPHVersion)
+    die("your php version must be { $minPHPVersion } or higher to run this app. your current version is" . phpversion());
+
 /** Path to this file */
 define('ROOTPATH', __DIR__ . DIRECTORY_SEPARATOR);
 
-echo ROOTPATH;
 require '../app/core/init.php';
 DEBUG ? ini_set('display_errors', 1) : ini_set('display_errors', 0);
 $app = new App;
 $app->loadController();
+
